66ad67777db7895f396105ca86c52ac222e7dfda
yusufsonmez <yusufsonmez1995@gmail.com>
2022-10-09 00:40:48 +0300

    PartialView Added.

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -3,28 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+            public ActionResult Index()
+            {
+                var movie = new Movie { Name = "First" };
+                var randomMovie = new RandomMovieViewModel
+                {
+                    Movie = movie
+                };
+                return View(randomMovie);
+            }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //ViewBag.Message = "Your application description page.";
 
-            return View();
+            var movie = new Movie { Name = "Second" };
+            var randomMovie = new RandomMovieViewModel
+            {
+                Movie = movie
+            };
+
+            return View(randomMovie);
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            //ViewBag.Message = "Your contact page.";
 
-            return View();
+            var movie = new Movie { Name = "Third" };
+            var randomMovie = new RandomMovieViewModel
+            {
+                Movie = movie
+            };
+
+            return View(randomMovie);
         }
     }
 }
