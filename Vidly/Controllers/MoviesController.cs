6c11c96f5f0bef0dd4585f4d83a37fc9e7d19403
yusufsonmez <yusufsonmez1995@gmail.com>
2022-10-08 23:50:30 +0300

    View Models

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,13 +14,20 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie(){ Name = "Shrek!" };
-            return View(movie);
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name="Yusuf"},
+                new Customer {Name="Aslı"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
 
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
     }
 }
