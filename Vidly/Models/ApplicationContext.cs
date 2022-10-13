a453fedfd1cf2e0f57be68f3a5b47d19f9e2502e
yusufsonmez <yusufsonmez1995@gmail.com>
2022-10-13 19:06:41 +0300

    Code-first , eager loading

---
+++ Vidly/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+
+}
