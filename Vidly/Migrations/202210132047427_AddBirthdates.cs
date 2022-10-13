60ba7db25ce802ae02e53ac4d7817930622affd8
yusufsonmez <yusufsonmez1995@gmail.com>
2022-10-13 23:57:53 +0300

    Exercise - Add birthdate to customer

---
+++ Vidly/Migrations/202210132047427_AddBirthdates.cs
@@ -0,0 +1,19 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthdates : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Customers (Name, IsSubscribedToNewsletter, MembershipTypeId, Birthdate) VALUES ('Aslı Yılmaz', 1, 1, 2000/01/01)");
+            Sql("INSERT INTO Customers (Name, IsSubscribedToNewsletter, MembershipTypeId, Birthdate) VALUES ('Melih Yıldız', 0, 2, 1998/01/01)");
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
