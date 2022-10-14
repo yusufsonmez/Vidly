fe20f8f24ef5f6c729a3f42ba5183d76f9cd48fb
yusufsonmez <yusufsonmez1995@gmail.com>
2022-10-14 20:14:22 +0300

    Input labels added

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -16,6 +16,10 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+        public ActionResult New()
+        {
+            return View();
+        }
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
