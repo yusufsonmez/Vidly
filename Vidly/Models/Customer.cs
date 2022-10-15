35b17794b9e0f92a609ab22641e22ba32985b6c4
yusufsonmez <yusufsonmez1995@gmail.com>
2022-10-15 15:25:02 +0300

    Custom validation added.

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,7 +9,7 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
@@ -17,6 +17,7 @@ namespace Vidly.Models
         [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
 }
